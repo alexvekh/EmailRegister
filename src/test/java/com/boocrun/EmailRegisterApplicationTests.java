8ba81376a1158e569dedbb65a50c0df5d54db214
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2022-12-29 23:42:48 -0500

    app created

---
+++ src/test/java/com/boocrun/EmailRegisterApplicationTests.java
@@ -0,0 +1,13 @@
+package com.boocrun;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailRegisterApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
